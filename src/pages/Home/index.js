2cf5dcd37077934b42c9ff0db2184f173e783f41
Bruno Brian da Silva Sousa <bruno.brian@BRSPOAMEANM0005.local>
2020-07-29 16:22:52 -0300

    adding permissions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
 import logo from '../../logo.svg';
 
 function App(props) {
+  const [users, setUsers] = useState([]);
+  const [count, setCount] = useState(0);
+  const [query, setQuery] = useState('');
+
+  const getUsers = useCallback((() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/`).then(response => {
+      return response.json();
+    }).then(data => {
+      setUsers(data);
+    })
+  }), []);
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Home - Page released for all
-        </p>
-      </header>
+      <div className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>UseMemo</h1>
+        <input onChange={e => setQuery(e.target.value)} />
+        <button onClick={() => setCount(count => count + 1)}>Increment</button>
+        <UserList users={users} query={query} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+function UserList({query, users}) {
+  const filtered = useMemo(() => filterUsers(users, query), [query, users]);
+
+  return filtered.map(user => 
+    <p style={{margin: 0}} key={user.id}>{user.name}</p>
+  )
+}
+
+const filterUsers = (users, query) => {
+  console.log('--------Filter function called--------')
+  return users.filter(user => user.name.toLowerCase().includes(query));
+}
--- src/pages/PublicPage/index.js
+++ src/pages/PublicPage/index.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect, useCallback, useMemo} from 'react';
 
 function ProtectAdmin() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count => count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count => count - 1);
+  }
+
+  useEffect(() => {
+    console.log('ProtectAdmin Re-render')
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'https://image.flaticon.com/icons/png/512/1169/1169904.png'} className="App-logo" alt="logo" />
-        <p>
-          Page released for all
-        </p>
+        <h1>UseCallback</h1>
+        <h2>Count: {count}</h2>
+        {/* useCallback para não atualizar a referencia do onCLick, então a função onclick é memorizada e não renderizara quando o componente for renderizado novamente. Mantém a igualdade referencial entre renders */}
+        <ButtonCount onClick={useCallback(() => increment(), [])}>Increment</ButtonCount>
+        <ButtonCount onClick={useCallback(() => decrement(), [])}>Decrement</ButtonCount>
+        {/* <ButtonCount onClick={decrement}>Decrement</ButtonCount> */}
       </header>
     </div>
   );
 }
 
 export default ProtectAdmin;
+
+
+function ButtonCount({...props}) {
+
+  useEffect(() => {
+    console.log("Render always component's father state was changed")
+  })
+
+  useEffect(() => {
+    console.log('Render button first time')
+  }, [])
+
+  useEffect(() => {
+    console.log('Button onClick Re-render')
+  }, [props.onClick])
+
+  return (
+    <div className="App">
+      <button {...props} />
+    </div>
+  );
+}
